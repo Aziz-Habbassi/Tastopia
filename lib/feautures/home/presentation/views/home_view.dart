c750001e1ff0a1204ef6224c489e39a76cbcb5ad
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-07-09 13:57:30 +0100

    add home view with video background

--- lib/feautures/home/presentation/views/home_view.dart
+++ lib/feautures/home/presentation/views/home_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tastopia/feautures/home/presentation/widgets/background_video_player.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: BackgroundVideoPlayer());
   }
 }
