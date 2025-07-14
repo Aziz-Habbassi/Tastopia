0672b6cc3a7c6b8dd1347bad8f5482551a6a6c52
Aziz Habbassi <azizhabbassi.online@gmail.com>
2025-07-14 11:42:43 +0100

    add title and image to meal details view

--- lib/feautures/meal_details/presentation/views/meal_details.dart
+++ lib/feautures/meal_details/presentation/views/meal_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MealDetails extends StatelessWidget {
   const MealDetails({super.key});
@@ -8,7 +9,12 @@ class MealDetails extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+        leading: IconButton(
+          onPressed: () {
+            context.go("/MealsView");
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
         title: Text(
           "Let's Cook together",
           style: TextStyle(
@@ -19,7 +25,37 @@ class MealDetails extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 0, 0, 0),
         centerTitle: true,
       ),
-      body: ListView(),
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * .25,
+              vertical: 50,
+            ),
+
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  spreadRadius: -10,
+                  blurRadius: 50,
+                ),
+              ],
+            ),
+            child: Image.asset("assets/images/hamburger.png"),
+          ),
+          Text(
+            "Hamburger",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontFamily: "Poppins", fontSize: 48),
+          ),
+          Text(
+            "ground beef, a hamburger bun,tomato, onion, and cheese",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontFamily: "Gt-Sectra", fontSize: 20),
+          ),
+        ],
+      ),
     );
   }
 }
